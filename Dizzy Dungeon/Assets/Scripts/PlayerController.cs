c8672e03c0b62efa5934b5c0023e937a82909553
tannypetepete <77975650+tannypetepete@users.noreply.github.com>
2023-02-22 17:07:57 -0500

    Update PlayerController.cs

--- Dizzy Dungeon/Assets/Scripts/PlayerController.cs
+++ Dizzy Dungeon/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Sprite playerSprite;
+    private bool canMove = true;
+
    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+
+    void MoveUp()
+    {
+
+    }
+
+    void MoveDown()
+    {
+
+    }
+    void MoveLeft()
+    {
+
+    }
+
+    void MoveRight()
+    {
+
+    }
+    void OnCollisionEnter(Collision OtherObject){
+        canMove = false;
     }
 }
