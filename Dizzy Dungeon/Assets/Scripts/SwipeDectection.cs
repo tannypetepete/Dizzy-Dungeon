7a7ae21b0e16e441b2fc21800211e3daf02df2e1
mossdragon <kaulin24@gmail.com>
2023-02-21 23:51:32 -0500

    Level One V0.1
    -Swipe detection is in, there may be some issues we need to work out on mobile.
    -The first level is build onto it's own playscene, poke around to see how I've built everything and we can probably replicate it easily
    -The player... exists. So far it has no movement (time constraints), and is basically a brick.
    -The environment should have colliders, with the flag and spikes having triggers instead.

---
+++ Dizzy Dungeon/Assets/Scripts/SwipeDectection.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDectection : MonoBehaviour
+{
+    public PlayerController player;
+    private Vector2 startPos;
+    public int swipeSensitive = 120; //# of pixels needed to detect a swipe
+    private bool fingerDown;
+    private bool mouseDown;
+
+    private void Update()
+    {
+       /* if (fingerDown = false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            startPos = Input.touches[0].position;
+            fingerDown = true;
+        }
+       */
+
+        //FOR TESTING, MOUSE INPUT FOR SWIPES;
+        if (mouseDown == false && Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+            mouseDown = true;
+            Debug.Log("Mouse Swipe");
+        }
+
+        if (fingerDown)
+        {
+            if (Input.touches[0].position.y >= startPos.y + swipeSensitive)
+            {
+                fingerDown = false;
+                Debug.LogWarning("Swipe Up! Implement Player Movement.");
+            }
+            if (Input.touches[0].position.y <= startPos.y - swipeSensitive)
+            {
+                fingerDown = false;
+                Debug.LogWarning("Swipe Down! Implement Player Movement.");
+            }
+
+            if (Input.touches[0].position.x >= startPos.x + swipeSensitive)
+            {
+                fingerDown = false;
+                Debug.LogWarning("Swipe Right! Implement Player Movement.");
+            }
+            if (Input.touches[0].position.x <= startPos.x - swipeSensitive)
+            {
+                fingerDown = false;
+                Debug.LogWarning("Swipe Left! Implement Player Movement.");
+            }
+        }
+
+        if (mouseDown)
+        {
+            if (Input.mousePosition.y >= startPos.y + swipeSensitive)
+            {
+                mouseDown = false;
+                Debug.LogWarning("Swipe Up! Implement Player Movement.");
+            }
+            if (Input.mousePosition.y <= startPos.y - swipeSensitive)
+            {
+                mouseDown = false;
+                Debug.LogWarning("Swipe Down! Implement Player Movement.");
+            }
+
+            if (Input.mousePosition.x >= startPos.x + swipeSensitive)
+            {
+                mouseDown = false;
+                Debug.LogWarning("Swipe Right! Implement Player Movement.");
+            }
+            if (Input.mousePosition.x <= startPos.x - swipeSensitive)
+            {
+                mouseDown = false;
+                Debug.LogWarning("Swipe Left! Implement Player Movement.");
+            }
+        }
+
+        if(mouseDown && Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+
+    }
+}
